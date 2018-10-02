95f33d8807d95829bbb215095ef97aa3921845a9
ati-lz <lafzi.a@gmail.com>
2018-10-02 15:07:47 +0200

    new demultiplexing code and changes accordingle on Seqwell snakefile

---
+++ SAM_cellBC_demultiplexing.py
@@ -0,0 +1,39 @@
+import os
+import sys
+
+big_sam_file = open(sys.argv[1], "r")
+bc_file = open(sys.argv[2], "r")
+protocol = sys.argv[3]
+sample = sys.argv[4]
+output_path = sys.argv[5]
+
+
+bc_list = []
+dict_demux = {}
+for line in bc_file:
+    bc = line.split()[0]
+    if bc != 'XC':
+        bc_list.append(bc)
+        dict_demux[bc] = []
+
+header_list = []
+for line in big_sam_file:
+    if line.startswith("@"):
+        header_list.append(line)
+    else:
+        line_words = line.split()
+        BC_tag = [i for i in line_words if i.startswith('XC')][0]
+        cellBC = BC_tag.split(":")[2]
+        if cellBC in dict_demux.keys():
+            dict_demux[cellBC].append(line)
+            
+for cBC in bc_list:
+    filenames = output_path + "/" + protocol + "." + "mixed." + sample + "." + cBC + ".sam"
+    fout = open(filenames, 'w')
+    for hline in header_list:
+        fout.write(hline)
+    for mapped_line in dict_demux[cBC]:
+        fout.write(mapped_line)
+    fout.close()
+        
+        
