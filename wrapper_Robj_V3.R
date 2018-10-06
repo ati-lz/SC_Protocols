731416d36c3af2c176861af5cefffb5d0555782e
ati-lz <lafzi.a@gmail.com>
2018-10-06 11:51:59 +0200

    testing sth about the wrapper

--- wrapper_Robj_V3.R
+++ wrapper_Robj_V3.R
@@ -239,7 +239,7 @@ main <- function(hsapExp,mmusExp,hnReads,hnUMI,hnGene,hnFeatures,mnReads,mnUMI,m
   print(colnames(all.mmusExp)[1:10])
   print(length(rownames(all.mmus.metadata)))
   print(rownames(all.mmus.metadata)[1:10])
-  full.SCE.mmus <- SingleCellExperiment(assays = list(counts = as.matrix(all.mmusExp)), colData = all.mmus.metadata[colnames(all.hsapExp),])
+  full.SCE.mmus <- SingleCellExperiment(assays = list(counts = as.matrix(all.mmusExp)), colData = all.mmus.metadata[colnames(all.mmusExp),])
   full.SCE.mmus <- calculateQCMetrics(full.SCE.mmus)
   libsize.drop.mmus <- isOutlier(full.SCE.mmus$total_counts, nmads=3, type="lower", log=TRUE)
   feature.drop.mmus <- isOutlier(full.SCE.mmus$total_features, nmads=3, type="lower", log=TRUE)
